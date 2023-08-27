77f85ad78095b3681b46937d8e8d8202a8da6d04
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2023-08-28 00:22:01 +0530

    0.2

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,12 +1,32 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './pages/Login';
+import { getTokenFromUrl } from './spotify';
 
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token; 
+    if(_token){
+      setToken(_token);
+    }
+    console.log('I HAVE A TOKEN',token)
+  } , []);
+
   return (
     <div className="App">
-      <Login/>
+      {
+        token ?(
+          <h1>i am logged in  </h1>
+        ) : (
+          <Login/>
+        )
+      }
+     
     </div>
   );
 }
--- spotify-clone-app/src/spotify.js
+++ spotify-clone-app/src/spotify.js
@@ -1,6 +1,31 @@
+// export const authEndpoint = "https://accounts.spotify.com/authorize";
+// const redirectUri = "http://localhost:3000/";
+// const clientId = 'f8faab10735a4171838ac6233a65526f';
+// const scopes = [
+//   "user-read-current-playing",
+//   "user-read-recently-played",
+//   "user-read-playback-state",
+//   "user-top-read",
+//   "user-modify-playback-state",
+// ];
+
+// export const getTokenFromUrl = () => {
+//   return window.location.hash
+//     .substring(1)
+//     .split("&")
+//     .reduce((initial, item) => {
+//       let parts = item.split("=");
+//       initial[parts[0]] = decodeURIComponent(parts[1]);
+//       return initial;
+//     }, {});
+// };
+// export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join(
+//   "%20"
+// )}
+// &response_type=token&show_dialog=true`;
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000/";
-const clientId = "f8faab10735a4171838ac6233a65526f";
+const redirectUri = "http://localhost:3000/"; // Make sure this matches the redirect URI in your Spotify Developer Dashboard
+const clientId = 'f8faab10735a4171838ac6233a65526f';
 const scopes = [
   "user-read-current-playing",
   "user-read-recently-played",
@@ -19,7 +44,4 @@ export const getTokenFromUrl = () => {
       return initial;
     }, {});
 };
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join(
-  "%20"
-)}
-&response_type=token&show_dialog=true`;
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes.join(" "))}&response_type=token&show_dialog=true`;
